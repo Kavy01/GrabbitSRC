d5c84c41f3ad22a194b5dcc1bf55b96ad7b288c6
Kavyansh Singh <81253296+Kavy01@users.noreply.github.com>
2023-11-13 11:39:11 +0530

    Add files via upload

---
+++ src/app/return-book/return-book.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { ApiService } from '../services/api.service';
+
+@Component({
+  selector: 'return-book',
+  templateUrl: './return-book.component.html',
+  styleUrls: ['./return-book.component.css'],
+})
+export class ReturnBookComponent implements OnInit  {
+  status: string = '';
+  bookForm: FormGroup;
+  username?: string;
+  user!: any;
+
+  constructor(public api: ApiService, private fb: FormBuilder) {
+    this.bookForm = this.fb.group({
+      bookId: fb.control('', [Validators.required]),
+      userId: fb.control('', [Validators.required]),
+    });
+  }
+  ngOnInit(): void {
+    console.log(this.api.getTokenUserInfo());
+    this.user = this.api.getTokenUserInfo()
+    
+   this.username = this.user.firstName;
+  }
+
+ 
+  returnBook() {
+    let book = (this.bookForm.get('bookId') as FormControl).value;
+    let user = this.user.id;
+    this.api.returnBook(book, user).subscribe({
+      next: (res: any) => {
+        console.log(res);
+        this.status = 'Book Returned!';
+        
+      },
+      error: (err: any) => console.log(err),
+    });
+  }
+}
